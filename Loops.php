ee9bb934985ff22a9930c0f72aacbc027f266553
AubTuv <152267051+AubTuv@users.noreply.github.com>
2024-10-08 10:44:27 +0800

    Act8

--- Loops.php
+++ Loops.php
@@ -22,14 +22,53 @@ do{
 }while($input != "password123");
 echo "Access Granted";
 echo "/n";
-//Act3
-for ($x = 0; $x <= 10; $x++) {
-    echo "7 x ".$x." =".$x*7."/n";
-  }
 
+//Act3
+<?php
+for ($x = 1; $x <= 10; $x++) {
+	$z=$x*7;
+    echo "7 x ".$x." =".$z."/n";
+  }
 ?>
 
 //Act 4
+for ($y = 1; $y <= 10; $y++) {    
+	if ($y==5)continue;
+    if ($y==8)break;
+    echo $y." ";
+
+  }
+
+//Act 5
+$number=1;
+$sum=0;
+while($number<=100){
+    $sum=$sum+$number;
+    $number++;
+}
+echo "The sum of numbers from 1 to 100 is: ".$sum;
+
+//Act6
+$bullet=1;
+$movie = array("Harry potter", "John Wick", "The Matrix", "Django", "Avatar");
+foreach ($movie as $a) {
+  echo $bullet.". $a /n";
+    $bullet++;
+}
+
+//Act7
+$identity = array("Name"=>"Alice", "Age"=>"20", "Grade"=>"A", "City"=>"Baguio");
+
+foreach ($identity as $b => $c) {
+  echo "$b : $c </n";
+}
+
+//Act8
+
+
+?>
+
+
 
 
 </body>
