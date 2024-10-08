729debdb1ac96d820056dddb362bd5fbf57dd827
AubTuv <152267051+AubTuv@users.noreply.github.com>
2024-10-08 10:11:24 +0800

    Activity2

--- Loops.php
+++ Loops.php
@@ -6,6 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-    
+<?php
+//Act1
+$counter=1;
+while($counter<=20){
+    if($counter%2==0){
+        echo $counter." ";
+    }$counter++;
+}
+
+$input=readline("Please enter the password: ");
+
+//Act2
+do{
+    $input=readline("Please enter the password: ");
+}while($input != "password123");
+echo "Access Granted";
+?>
 </body>
 </html>
